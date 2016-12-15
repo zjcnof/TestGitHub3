ddc5bbbe5eca816cb3b245c0ec2758076d192d7a
ZJC <zjcnof@163.com>
2016-12-15 14:03:40 +0800

    B3，敲了3个回车

--- app/src/main/java/com/zjc/testgithub3/MainActivity.java
+++ app/src/main/java/com/zjc/testgithub3/MainActivity.java
@@ -13,6 +13,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+
         Log.i(TAG, "onCreate: B2更改");
     }
 }
