c132ee8c6e48c72cf73ebbead402f02e44e869f2
ZJC <zjcnof@163.com>
2016-12-15 13:59:42 +0800

    A2更改了

--- app/src/main/java/com/zjc/testgithub3/MainActivity.java
+++ app/src/main/java/com/zjc/testgithub3/MainActivity.java
@@ -13,6 +13,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.i(TAG, "onCreate: 对的");
+        Log.i(TAG, "onCreate: A2更改");
     }
 }
